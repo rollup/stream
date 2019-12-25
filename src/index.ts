0c90815a04351d1e9485814008ed2604ccd18d3d
Tiger Oakes <toakes@mozilla.com>
2019-12-24 17:27:04 -1000

    chore: tweak tests and types (#2)

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,37 @@ import { Readable } from 'stream';
 import { rollup, RollupOptions } from 'rollup';
 
 const build = async (options: RollupOptions, stream: Readable) => {
-  try {
-    const bundle = await rollup(options);
+  const bundle = await rollup(options);
 
-    stream.emit('bundle', bundle);
+  stream.emit('bundle', bundle);
 
-    const { output } = await bundle.generate(options);
+  const { output } = await bundle.generate(options);
 
-    // OuputChunk | OutputAsset. TypeScript can't destructure a union type.
-    for (const { code, isAsset, map, source } of output as any) {
-      if (isAsset) {
-        stream.push(source);
-        continue; // eslint-disable-line no-continue
-      }
+  for (const chunk of output) {
+    if (chunk.type === 'asset') {
+      stream.push(chunk.source);
+    } else {
+      stream.push(chunk.code);
 
-      stream.push(code);
-
-      if (map) {
-        stream.push(`\n//# sourceMappingURL=${map.toUrl()}`);
+      if (chunk.map) {
+        stream.push(`\n//# sourceMappingURL=${chunk.map.toUrl()}`);
       }
     }
-
-    // signal end of write
-    stream.push(null);
-  } catch (error) {
-    stream.emit('error', error);
   }
+
+  // signal end of write
+  stream.push(null);
 };
 
 const stream = (options: RollupOptions) => {
-  const result = new Readable();
+  const result = new Readable({
+    // stub _read() as it's not available on Readable stream, needed by gulp et al
+    read: () => {}
+  });
 
-  // stub _read() as it's not available on Readable stream, needed by gulp et al
-  result._read = () => {}; // eslint-disable-line no-underscore-dangle
-
-  build(options, result).catch(() => {});
+  build(options, result).catch((error) => {
+    result.emit('error', error);
+  });
 
   return result;
 };
