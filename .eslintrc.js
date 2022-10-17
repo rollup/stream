f1808a22eecdebe5df1513c1188c5675a39f3658
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-10-17 06:07:35 +0200

    fix: update rollup peer dependency
    BREAKING CHANGES: Requires Node 14

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,29 @@
 module.exports = {
   extends: ['rollup', 'plugin:import/typescript'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['./tsconfig.eslint.json', './tsconfig.json'],
+        tsconfigRootDir: __dirname
+      },
+      rules: { '@typescript-eslint/no-unnecessary-type-assertion': 'error' }
+    }
+  ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.eslint.json', './tsconfig.json'],
-    tsconfigRootDir: __dirname
-  },
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/consistent-type-assertions': 'error',
     '@typescript-eslint/consistent-type-definitions': 'error',
     '@typescript-eslint/member-ordering': 'error',
     '@typescript-eslint/no-inferrable-types': 'error',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        vars: 'local',
         args: 'after-used',
-        ignoreRestSiblings: true
+        ignoreRestSiblings: true,
+        vars: 'local'
       }
     ],
     'import/extensions': [
@@ -30,17 +36,17 @@ module.exports = {
         tsx: 'never'
       }
     ],
-    'import/no-namespace': 'off',
     'import/no-named-export': 'off',
+    'import/no-namespace': 'off',
     'no-unused-vars': 'off',
     'prettier/prettier': [
       'error',
       {
         arrowParens: 'always',
+        plugins: ['prettier-plugin-package'],
         printWidth: 100,
         singleQuote: true,
-        trailingComma: 'none',
-        plugins: ['prettier-plugin-package']
+        trailingComma: 'none'
       }
     ]
   }
